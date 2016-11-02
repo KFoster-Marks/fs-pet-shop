64edf873b23bcb13953dbb80bd5ef42b8c1f798e
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-11-02 07:02:21 -0700

    task one display usage exit process

--- pets.js
+++ pets.js
@@ -9,43 +9,11 @@ var file = path.basename(process.argv[1]);
 var cmd = process.argv[2];
 
 if (cmd === 'read') {
-  fs.readFile(petsPath, 'utf8', function(err, data) {
-    if (err) {
-      throw err;
-    }
-
-    var pets = JSON.parse(data);
-
-    console.log(pets);
-  });
+  console.log('the user commanded we read!');
 }
 
 else if (cmd === 'create') {
-  fs.readFile(petsPath, 'utf8', function(readErr, data) {
-    if (readErr) {
-      throw readErr;
-    }
-
-    var pets = JSON.parse(data);
-    var pet = process.argv[3];
-
-    if (!pet) {
-      console.error(`Usage: ${node} ${file} ${cmd} pet`);
-      process.exit(1);
-    }
-
-    pets.push(pet);
-
-    var petsJSON = JSON.stringify(pets);
-
-    fs.writeFile(petsPath, petsJSON, function(writeErr) {
-      if (writeErr) {
-        throw writeErr;
-      }
-
-      console.log(pet);
-    });
-  });
+  console.log('the user commanded we create!');
 }
 
 else if (cmd === 'update') {
