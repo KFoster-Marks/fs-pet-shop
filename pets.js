bef2825181b8a246b1d7d0a9a080cbbf77464139
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-11-02 07:27:52 -0700

    fourth task exit process

--- pets.js
+++ pets.js
@@ -8,13 +8,10 @@ var node = path.basename(process.argv[0]);
 var file = path.basename(process.argv[1]);
 var cmd = process.argv[2];
 
-//Additionally, your application must handle the read subcommand when given an index. In this case, it must read the pets.json file, parse its data to a native JavaScript object, access the correct record, and log it to the console. If the call to the filesystem fails for any reason, it should throw the resulting error.
+//Additionally, your application must handle the read subcommand when given an out-of-bound index. In this case, it must display a more specific usage to the standard error channel and exit with a non-zero exit code.
 
 if (cmd === 'read') {
-  console.log('the user commanded we read!');
-
   fs.readFile(petsPath, 'utf8', function(err, data) {
-
     if (err) {
       throw err;
     }
@@ -23,6 +20,7 @@ if (cmd === 'read') {
 
     if (process.argv[3] && pets.length <= process.argv[3]) {
       console.log('the array ain\'t that long, fool!');
+      process.exit(1);
     } else if (process.argv[3]) {
       console.log(pets[process.argv[3]]);
     }  else {
