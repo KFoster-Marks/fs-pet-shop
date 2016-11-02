6d90e0d3dc01ff179a94b129467bd5ed0d615d09
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-11-02 07:24:25 -0700

    third task parse by index

--- pets.js
+++ pets.js
@@ -5,11 +5,10 @@ var path = require('path');
 var petsPath = path.join(__dirname, 'pets.json');
 
 var node = path.basename(process.argv[0]);
-console.log(process.argv);
 var file = path.basename(process.argv[1]);
 var cmd = process.argv[2];
 
-//Your next task is to refactor the application to handle the read subcommand via the process arguments, read the pets.json file, parse its data to a native JavaScript object, and log it to the console. If the call to the filesystem fails for any reason, it should throw the resulting error.
+//Additionally, your application must handle the read subcommand when given an index. In this case, it must read the pets.json file, parse its data to a native JavaScript object, access the correct record, and log it to the console. If the call to the filesystem fails for any reason, it should throw the resulting error.
 
 if (cmd === 'read') {
   console.log('the user commanded we read!');
@@ -21,7 +20,14 @@ if (cmd === 'read') {
     }
 
     var pets = JSON.parse(data);
-    console.log(pets);
+
+    if (process.argv[3] && pets.length <= process.argv[3]) {
+      console.log('the array ain\'t that long, fool!');
+    } else if (process.argv[3]) {
+      console.log(pets[process.argv[3]]);
+    }  else {
+      console.log(pets);
+    }
   });
 }
 
